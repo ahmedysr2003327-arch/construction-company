3b4d3785cf87a7509f6a8fc63ae315784179dd0f
Ahmed Yaser <ahmedysr777@gmail.com>
2026-07-29 17:11:32 +0300

    changes from google

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,16 +3,16 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://your-domain.com",
+      url: "https://kanouz-alzooq.com",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://your-domain.com/projects",
+      url: "https://kanouz-alzooq.com/projects",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
   ];
 }
